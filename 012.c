0d678572fe49ae0ec942eee27332c67e575fb783
crystlee1005 <crystlee1005@sookmyung.ac.kr>
2020-11-19 16:00:42 +0900

    실습5

--- 012.c
+++ 012.c
@@ -3,15 +3,26 @@
 #include <string.h>
 
 
-void main(void) {
+int main ( int argc, char *argv[]) {
 	
-  char src[] = "The worst things to eat before you sleep" ;
-  char dst[100];
-  char str[30] = "happy C programming" ;
+  FILE *fp;
+  char str[100];
+  int i;
   
-  strcpy(dst, src);
-  printf("copied string : %s \n", dst);
-  printf(" length of \%s\ : %i \n", str, strlen(str) );
+  fp = fopen("sample.txt", "w");
+  if ( fp == NULL)
+  {
+  	return -1;
+  }
+  
+  for(i=0; i<3; i++)
+  {
+  	printf("Input a word: ");
+  	scanf("%s", str);
+  	fprintf(fp, "%s\n", str);
+  }
+  fclose(fp);
+  
   return 0;
 }
 	
